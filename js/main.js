61c597a70767455bfbd248d2410ae56db34d8967
bakhodirsuyarov <bakhodirsodiqovich661@gmail.com>
2022-11-23 17:55:41 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+// console.log(Math.floor(Math.random() * 10));
+
+
+let number1 = Number(prompt("Birinchi sonni kiriting"));
+let number2 = Number(prompt("Ikkinchi sonni kiriting"));
+
+console.log(number1 * number2);
+console.log(number1 + number2);
+console.log(Math.pow(number1, 2));
+console.log(Math.pow(number2, 2));
+
+
+// let Massa = Number(prompt("Massa kiriting : "))
+
+// console.log(Massa / 1000 +  " tonna")
+
+
+// let a = 5;
+// let b = 6;
+// let c
+
+
+// console.log(a,b)
+
+// c = a
+
+// a = b
+
+// b = c
+
+// console.log(b,a)
+
+
+// let soat = Number(prompt("Soat kiriting"))
+
+cons ole.log(soat * 60 + " daqiqa   " + soat * 3600 + " sekund")
+
+
+
+
+
+
+
+
+// let num = Number(prompt("sonni kiriting : "))
+
+// if(num%3 ==0 && num%5 ==0){
+//   console.log("FizzBuzz")
+// }
+// else if(num%3 ==0){
+//   console.log("Fizz")
+// }else if(num%5 ==0){
+//   console.log("Buzz")
+// }
+// else{
+//   console.log("Bu son 3 ga ham 5 ga ham bulinmedi")
+// }
+
+
+
+
+
+let a = parseInt(prompt("sonni kiriting:"))
+
+if(a%2 ==0){
+  console.log("Juft son")
+}else if(a%2 ==1){
+  console.log("Toq son")
+}
